6ff0efcd1dc0fc810e81dffe2d68d96a2411592c
Sanjay1882006 <110036072+Sanjay1882006@users.noreply.github.com>
2022-12-21 17:03:22 +0530

    Add files via upload

---
+++ use_con_str_to_int.py
@@ -0,0 +1,8 @@
+# use module 
+import convert_str_to_int as csi
+str_list=['1','2','3','4','5']
+csi.str_to_int(str_list)
+int_list=csi.int_list
+int_list2=[6,7,8,9,0]
+sum_list=int_list[0]+int_list2[0]
+print(sum_list)
